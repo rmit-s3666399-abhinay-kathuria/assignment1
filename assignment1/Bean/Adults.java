59d368cd4a1d7305e67fd0b1f2c8e570fadb9359
Abhi <s3666399@student.rmit.edu.au>
2018-03-23 16:28:35 +1100

    Class Adult

---
+++ assignment1/Bean/Adults.java
@@ -0,0 +1,13 @@
+package Bean;
+
+public class Adults extends User {
+
+    public Adults(String name, int age) {
+        super(name, age);
+    }
+
+    public Adults() {
+        super();
+    }
+}
+
